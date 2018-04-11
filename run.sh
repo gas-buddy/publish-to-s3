4dbfc4e4b5d9734c0fa5ddfc79fcea2714d3d1c8
Jean-Charles Sisk <jasisk@gmail.com>
2018-04-11 15:41:18 -0400

    add loglevel to wercker vars

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 BUCKET="${WERCKER_PUBLISH_TO_S3_BUCKET}"
 SRC="${WERCKER_PUBLISH_TO_S3_SRC}"
 ROLE="${WERCKER_PUBLISH_TO_S3_ROLE}"
+LOGLEVEL="${WERCKER_PUBLISH_TO_S3_LOGLEVEL:-"info"}"
 
 echo "Publishing to bucket ${BUCKET} ..."
-exec node "${WERCKER_STEP_ROOT}/cli.js" -b "${BUCKET}" -s "${SRC}" -r "${ROLE}"
+exec node "${WERCKER_STEP_ROOT}/cli.js" -l "${LOGLEVEL}" -b "${BUCKET}" -s "${SRC}" -r "${ROLE}"
