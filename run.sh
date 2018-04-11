77cf5cdf7914b6c6347c085fa8914193609be385
Jean-Charles Sisk <jasisk@gmail.com>
2018-04-11 16:37:12 -0400

    no eval in wercker

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ ROLE="${WERCKER_PUBLISH_TO_S3_ROLE}"
 LOGLEVEL="${WERCKER_PUBLISH_TO_S3_LOGLEVEL:-"info"}"
 
 echo "Publishing to bucket ${BUCKET} ..."
-exec node "${WERCKER_STEP_ROOT}/cli.js" -l "${LOGLEVEL}" -b "${BUCKET}" -s "${SRC}" -r "${ROLE}"
+node "${WERCKER_STEP_ROOT}/cli.js" -l "${LOGLEVEL}" -b "${BUCKET}" -s "${SRC}" -r "${ROLE}" || exit 1
