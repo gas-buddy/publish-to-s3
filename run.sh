3d2377e6fe40f9082785af1dfd204e8820a52c66
Jean-Charles Sisk <jasisk@gmail.com>
2018-04-11 14:55:21 -0400

    exec for exit code

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ BUCKET="${WERCKER_PUBLISH_TO_S3_BUCKET}"
 SRC="${WERCKER_PUBLISH_TO_S3_SRC}"
 ROLE="${WERCKER_PUBLISH_TO_S3_ROLE}"
 
-node "${WERCKER_STEP_ROOT}/cli.js" -b "${BUCKET}" -s "${SRC}" -r "${ROLE}"
+echo "Publishing to bucket ${BUCKET} ..."
+exec node "${WERCKER_STEP_ROOT}/cli.js" -b "${BUCKET}" -s "${SRC}" -r "${ROLE}"
